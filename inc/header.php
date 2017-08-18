27026a197f892fbba212b109d0c7bfd9df99975b
Abdul Majeed Shehzad <shehzad3880@gmail.com>
2017-08-19 04:45:45 +0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -10,13 +10,12 @@ if (!isset($_SESSION['start'])) {
 <title>Message BroadCasting Service</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
 <link rel="stylesheet" href="css/w3-custom.css">
 <link rel="stylesheet" href="css/custom.css">
 <link rel="stylesheet" href="css/w3-theme-indigo.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
-<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="js/jquery-3.1.1.js"></script>
 <script src="js/script.js"></script>
 <style>
